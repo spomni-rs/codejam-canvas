324dfa815ebe98ea5f6f7bab7073b306c08a23c2
spomni <mikhail.snitko@gmail.com>
2019-10-27 19:58:15 +0300

    tool: Setup linters.

--- codejam-canvas/gulpfile.js/index.js
+++ codejam-canvas/gulpfile.js/index.js
@@ -3,3 +3,15 @@ const {series, parallel} = gulp;
 const requireDir = require('require-dir');
 
 requireDir('./tasks')
+
+gulp.task('lint', series(
+  'lint-html',
+  'lint-styles',
+  'lint-js'
+))
+
+gulp.task('lint--fix', series(
+  'lint-html',
+  'lint-styles--fix',
+  'lint-js--fix'
+))
