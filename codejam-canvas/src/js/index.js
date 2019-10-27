171a7aa2f5cc9a839d0949d30478ac7a913893f0
spomni <mikhail.snitko@gmail.com>
2019-10-27 19:39:07 +0300

    refactor: Clean code.

--- codejam-canvas/src/js/index.js
+++ codejam-canvas/src/js/index.js
@@ -1,10 +1,11 @@
 import style from '../scss/index.scss';
-
-// header
 import {MDCTopAppBar} from '@material/top-app-bar';
-
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-
 import Workspace from './lib/workspace';
-let workspace = new Workspace(document.querySelector('.workspace'))
+
+const topAppBar = new MDCTopAppBar(
+  document.querySelector('.mdc-top-app-bar')
+);
+
+const workspace = new Workspace(
+  document.querySelector('.workspace')
+);
